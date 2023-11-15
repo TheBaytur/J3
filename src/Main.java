f4638e1121f49c7ee8581628ee106686b36f8383
TheBaytur <bayturcodding@gmail.com>
2023-11-15 20:22:26 +0800

    Вызываем фигуры в мейнеб массив фигур

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Figure[] figures = new Figure[3];
+        figures[0] = new Circle(2);
+        figures[1] = new Rectangle(5,6);
+        figures[2] = new Triangle(3,4,5);
+
+        for (Figure figure : figures){
+            print("area: " + figure.getArea() + ", perimeter: " + figure.getPerimeter());
+        }
+    }
+
+    private static void print(String text){
+        System.out.println(text);
     }
 }
 
